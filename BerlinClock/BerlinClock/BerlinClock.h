4be3727993e99238d6c52e66fafc4af9ecd53964
2021-DEV2-012 <80991609+2021-DEV2-012@users.noreply.github.com>
2021-03-21 00:39:03 +0100

    Setup empty Xcode project with mac target for the development of the core logic

---
+++ BerlinClock/BerlinClock/BerlinClock.h
@@ -0,0 +1,18 @@
+//
+//  BerlinClock.h
+//  BerlinClock
+//
+//  Created by ***REMOVED*** on 21/03/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BerlinClock.
+FOUNDATION_EXPORT double BerlinClockVersionNumber;
+
+//! Project version string for BerlinClock.
+FOUNDATION_EXPORT const unsigned char BerlinClockVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BerlinClock/PublicHeader.h>
+
+
