b6d4bea6adf7aa7aa6cdb8a920b7a80318f48426
2021-DEV2-012 <80991609+2021-DEV2-012@users.noreply.github.com>
2021-03-21 00:39:03 +0100

    Setup empty Xcode project with mac target for the development of the core logic

---
+++ BerlinClock/BerlinClockTests/BerlinClockTests.swift
@@ -0,0 +1,12 @@
+//
+//  BerlinClockTests.swift
+//  BerlinClockTests
+//
+//  Created by Hugo Alonso on 21/03/2021.
+//
+
+import XCTest
+@testable import BerlinClock
+
+class BerlinClockTests: XCTestCase {
+}
