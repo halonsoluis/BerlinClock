2fc7f9f55fa08f8b853b107b50eb867ad6722331
2021-DEV2-012 <80991609+2021-DEV2-012@users.noreply.github.com>
2021-03-21 01:20:31 +0100

    Assert that for minute 1 of the day the result is Y000

--- BerlinClock/BerlinClockTests/BerlinClockTests.swift
+++ BerlinClock/BerlinClockTests/BerlinClockTests.swift
@@ -11,7 +11,15 @@ import XCTest
 final class BerlinClock {
 
     func singleMinuteRow(for date: Date) -> String {
-        return "0000"
+        let calendar = Calendar.init(identifier: .gregorian)
+
+        let minute = calendar.component(.minute, from: date)
+
+        if minute == 0 {
+            return "0000"
+        } else {
+            return "Y000"
+        }
     }
 
 }
@@ -25,5 +33,15 @@ final class BerlinClockTests: XCTestCase {
         let result = sut.singleMinuteRow(for: time!)
 
         XCTAssertEqual(result, "0000")
+    }
+
+    func test_singleMinuteRow_returnsYOOOforSecondMinuteOfTheDay() {
+        let sut = BerlinClock()
+        let calendar = Calendar.init(identifier: .gregorian)
+        let time = calendar.date(bySettingHour: 0, minute: 1, second: 0, of: Date())
+
+        let result = sut.singleMinuteRow(for: time!)
+
+        XCTAssertEqual(result, "Y000")
     }
 }
