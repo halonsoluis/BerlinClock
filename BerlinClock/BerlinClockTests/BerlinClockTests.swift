be9f97541f539ec1046ce184b5cfd8af38b00d67
2021-DEV2-012 <80991609+2021-DEV2-012@users.noreply.github.com>
2021-03-21 01:15:21 +0100

    Assert that at minute 0 the single minute row is 0000

--- BerlinClock/BerlinClockTests/BerlinClockTests.swift
+++ BerlinClock/BerlinClockTests/BerlinClockTests.swift
@@ -8,5 +8,22 @@
 import XCTest
 @testable import BerlinClock
 
-class BerlinClockTests: XCTestCase {
+final class BerlinClock {
+
+    func singleMinuteRow(for date: Date) -> String {
+        return "0000"
+    }
+
+}
+
+final class BerlinClockTests: XCTestCase {
+    func test_singleMinuteRow_returnsOOOOforFirstMomentOfTheDay() {
+        let sut = BerlinClock()
+        let calendar = Calendar.init(identifier: .gregorian)
+        let time = calendar.date(bySettingHour: 0, minute: 0, second: 0, of: Date())
+
+        let result = sut.singleMinuteRow(for: time!)
+
+        XCTAssertEqual(result, "0000")
+    }
 }
