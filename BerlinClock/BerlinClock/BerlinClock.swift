ad96e72fd5172dc38b37193fcb8e208acf0aeddd
2021-DEV2-012 <80991609+2021-DEV2-012@users.noreply.github.com>
2021-03-21 22:04:10 +0100

    Extract BerlinCoreEngine into own file

--- BerlinClock/BerlinClock/BerlinClock.swift
+++ BerlinClock/BerlinClock/BerlinClock.swift
@@ -7,47 +7,6 @@
 
 import Foundation
 
-final class BerlinClockEngine {
-    func secondsLamp(for second: Int) -> Bool {
-        second % 2 == 0
-    }
-
-    func fiveHourRow(for hour: Int) -> [Bool] {
-        calculateLights(
-            total: 4,
-            iluminated: hour / 5
-        )
-    }
-
-    func singleHourRow(for hour: Int) -> [Bool] {
-        calculateLights(
-            total: 4,
-            iluminated: hour % 5
-        )
-    }
-
-    func fiveMinuteRow(for minute: Int) -> [Bool] {
-        calculateLights(
-            total: 11,
-            iluminated: minute / 5
-        )
-    }
-
-    func singleMinuteRow(for minute: Int) -> [Bool] {
-        calculateLights(
-            total: 4,
-            iluminated: minute % 5
-        )
-    }
-
-    private func calculateLights(total amountOfLights: Int, iluminated: Int) -> [Bool] {
-        let onLights = Array(repeating: true, count: iluminated)
-        let offLights = Array(repeating: false, count: amountOfLights - iluminated)
-
-        return onLights + offLights
-    }
-}
-
 final class BerlinClock {
 
     struct ColorSchema {
