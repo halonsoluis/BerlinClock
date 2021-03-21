72355a0dd1b89a51c464b2a11edbef719d520034
2021-DEV2-012 <80991609+2021-DEV2-012@users.noreply.github.com>
2021-03-21 06:45:59 +0100

    Simplify helper functions making calling site more verbose.
    Avoid hiding intentions by removing the function evaluateFunctionRow after exposing the intention in call site

--- BerlinClock/BerlinClockTests/TestUtils.swift
+++ BerlinClock/BerlinClockTests/TestUtils.swift
@@ -9,64 +9,42 @@ import XCTest
 
 extension BerlinClockTests {
 
-    func assertSingleMinuteRow(every5MinutesAfter minute: Int, returns: String, file: StaticString = #file, line: UInt = #line) {
-        let (sut, time) = createSut(minute: minute)
+    func assertSingleMinuteRow(minutes: [Int], originalDate: Date = Date(), returns: String, file: StaticString = #file, line: UInt = #line) {
+        let (sut, calendar) = createSut()
 
-        let amountOfMultiplesOfFiveInAMinute = 60 / 5
-        let fiveMinutes: TimeInterval = 5 * 60
+        let result = Set(
+            minutes
+                .map { calendar.date(bySettingHour: 0, minute: $0, second: 0, of: originalDate)! }
+                .map(sut.singleMinuteRow)
+        )
+        
+        XCTAssertEqual(result.count, 1, file: file, line: line)
+        XCTAssertEqual(result.first!, returns, file: file, line: line)
+    }
 
-        let uniqueResults = evaluateFunctionRow(
-            function: sut.singleMinuteRow,
-            after: time,
-            every: fiveMinutes,
-            repetitions: amountOfMultiplesOfFiveInAMinute
+    func assertFiveMinuteRow(minutes: ClosedRange<Int>, returns: String, originalDate: Date = Date(), file: StaticString = #file, line: UInt = #line) {
+        let (sut, calendar) = createSut()
+
+        let result = Set(
+            minutes
+                .map { calendar.date(bySettingHour: 0, minute: $0, second: 0, of: originalDate)! }
+                .map(sut.fiveMinuteRow)
         )
 
-        XCTAssertEqual(uniqueResults.count, 1, file: file, line: line)
-        XCTAssertEqual(uniqueResults.first!, returns, file: file, line: line)
+        XCTAssertEqual(result.count, 1, "Expected a unique set of results", file: file, line: line)
+        XCTAssertEqual(result.first!, returns, file: file, line: line)
     }
 
-    func assertFiveMinuteRow(inTheNext4minutesAfter minute: Int, returns: String, file: StaticString = #file, line: UInt = #line) {
-        let (sut, time) = createSut(minute: minute)
-        let minute: TimeInterval = 60
+    func assertFiveHourRow(hours: ClosedRange<Int>, returns: String, originalDate: Date = Date(), file: StaticString = #file, line: UInt = #line) {
+        let (sut, calendar) = createSut()
 
-        let uniqueResults = evaluateFunctionRow(
-            function: sut.fiveMinuteRow,
-            after: time,
-            every: minute,
-            repetitions: 4
+        let result = Set(
+            hours
+                .map { calendar.date(bySettingHour: $0, minute: 0, second: 0, of: originalDate)! }
+                .map(sut.fiveHourRow)
         )
 
-        XCTAssertEqual(uniqueResults.count, 1, "Expected a unique set of results", file: file, line: line)
-        XCTAssertEqual(uniqueResults.first!, returns, file: file, line: line)
-    }
-
-    func assertFiveHourRow(hours: ClosedRange<Int>, returns: String, file: StaticString = #file, line: UInt = #line) {
-        let (sut, _) = createSut(hour: hours.lowerBound)
-
-        let result = hours.map { (hour) in
-            Calendar.init(identifier: .gregorian).date(bySettingHour: hour, minute: 0, second: 0, of: Date())!
-        }.map(sut.fiveHourRow)
-
-
-        let uniqueResults = Set(result)
-
-        XCTAssertEqual(uniqueResults.count, 1, "Expected a unique set of results", file: file, line: line)
-        XCTAssertEqual(uniqueResults.first!, returns, file: file, line: line)
-    }
-
-    func evaluateFunctionRow(function: (Date) -> String,
-                             after time: Date,
-                             every interval: TimeInterval,
-                             repetitions: Int) -> Set<String> {
-        var result = [String]()
-        var movingWindow = time
-
-        (0...repetitions).forEach { (_) in
-            result.append(function(movingWindow))
-            movingWindow = movingWindow.addingTimeInterval(interval)
-        }
-
-        return Set(result)
+        XCTAssertEqual(result.count, 1, "Expected a unique set of results", file: file, line: line)
+        XCTAssertEqual(result.first!, returns, file: file, line: line)
     }
 }
