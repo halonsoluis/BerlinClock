4a610b48317a12aca792777788864b7e40de2364
2021-DEV2-012 <80991609+2021-DEV2-012@users.noreply.github.com>
2021-03-21 06:45:59 +0100

    Simplify helper functions making calling site more verbose.
    Avoid hiding intentions by removing the function evaluateFunctionRow after exposing the intention in call site

--- BerlinClock/BerlinClockTests/BerlinClockTests.swift
+++ BerlinClock/BerlinClockTests/BerlinClockTests.swift
@@ -86,28 +86,32 @@ final class BerlinClockTests: XCTestCase {
     // MARK: - Single Minutes Row
     
     func test_singleMinuteRow_returnsExpectedOutput() {
-        assertSingleMinuteRow(every5MinutesAfter: 0, returns: "0000")
-        assertSingleMinuteRow(every5MinutesAfter: 1, returns: "Y000")
-        assertSingleMinuteRow(every5MinutesAfter: 2, returns: "YY00")
-        assertSingleMinuteRow(every5MinutesAfter: 3, returns: "YYY0")
-        assertSingleMinuteRow(every5MinutesAfter: 4, returns: "YYYY")
+        let sixtyCountsAs0 = 1
+        let cases = (60 / 5) - sixtyCountsAs0
+        let multip = (0...cases).map { $0 * 5 }
+
+        assertSingleMinuteRow(minutes: multip.map { $0 + 0 }, returns: "0000")
+        assertSingleMinuteRow(minutes: multip.map { $0 + 1 }, returns: "Y000")
+        assertSingleMinuteRow(minutes: multip.map { $0 + 2 }, returns: "YY00")
+        assertSingleMinuteRow(minutes: multip.map { $0 + 3 }, returns: "YYY0")
+        assertSingleMinuteRow(minutes: multip.map { $0 + 4 }, returns: "YYYY")
     }
 
     // MARK: - Five Minutes Row
 
     func test_fiveMinuteRow_returnsExpectedOutput() {
-        assertFiveMinuteRow(inTheNext4minutesAfter: 00, returns: "00000000000")
-        assertFiveMinuteRow(inTheNext4minutesAfter: 05, returns: "Y0000000000")
-        assertFiveMinuteRow(inTheNext4minutesAfter: 10, returns: "YY000000000")
-        assertFiveMinuteRow(inTheNext4minutesAfter: 15, returns: "YYR00000000")
-        assertFiveMinuteRow(inTheNext4minutesAfter: 20, returns: "YYRY0000000")
-        assertFiveMinuteRow(inTheNext4minutesAfter: 25, returns: "YYRYY000000")
-        assertFiveMinuteRow(inTheNext4minutesAfter: 30, returns: "YYRYYR00000")
-        assertFiveMinuteRow(inTheNext4minutesAfter: 35, returns: "YYRYYRY0000")
-        assertFiveMinuteRow(inTheNext4minutesAfter: 40, returns: "YYRYYRYY000")
-        assertFiveMinuteRow(inTheNext4minutesAfter: 45, returns: "YYRYYRYYR00")
-        assertFiveMinuteRow(inTheNext4minutesAfter: 50, returns: "YYRYYRYYRY0")
-        assertFiveMinuteRow(inTheNext4minutesAfter: 55, returns: "YYRYYRYYRYY")
+        assertFiveMinuteRow(minutes: 0...4, returns: "00000000000")
+        assertFiveMinuteRow(minutes: 5...9, returns: "Y0000000000")
+        assertFiveMinuteRow(minutes: 10...14, returns: "YY000000000")
+        assertFiveMinuteRow(minutes: 15...19, returns: "YYR00000000")
+        assertFiveMinuteRow(minutes: 20...24, returns: "YYRY0000000")
+        assertFiveMinuteRow(minutes: 25...29, returns: "YYRYY000000")
+        assertFiveMinuteRow(minutes: 30...34, returns: "YYRYYR00000")
+        assertFiveMinuteRow(minutes: 35...39, returns: "YYRYYRY0000")
+        assertFiveMinuteRow(minutes: 40...44, returns: "YYRYYRYY000")
+        assertFiveMinuteRow(minutes: 45...49, returns: "YYRYYRYYR00")
+        assertFiveMinuteRow(minutes: 50...54, returns: "YYRYYRYYRY0")
+        assertFiveMinuteRow(minutes: 55...59, returns: "YYRYYRYYRYY")
     }
 
     // MARK: - Five Hour Row
@@ -124,13 +128,11 @@ final class BerlinClockTests: XCTestCase {
 //MARK - Helpers
 extension BerlinClockTests {
 
-    func createSut(second: Int = 0, minute: Int = 0, hour: Int = 0, originalDate: Date = Date()) -> (BerlinClockRepresentation, Date) {
+    func createSut(second: Int = 0, minute: Int = 0, hour: Int = 0) -> (BerlinClockRepresentation, Calendar) {
         let calendar = Calendar.init(identifier: .gregorian)
         let sut = BerlinClock(calendar: calendar)
 
-        let time = calendar.date(bySettingHour: hour, minute: minute, second: second, of: originalDate)!
-
-        return (sut, time)
+        return (sut, calendar)
     }
 
 }
