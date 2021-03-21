097c4319c16e75f4a226ef2a069986a8ef23b420
2021-DEV2-012 <80991609+2021-DEV2-012@users.noreply.github.com>
2021-03-21 04:24:42 +0100

    Assert that the Five Minutes Row display "OOOOOOOOOOO" at minutes 0-4

--- BerlinClock/BerlinClockTests/BerlinClockTests.swift
+++ BerlinClock/BerlinClockTests/BerlinClockTests.swift
@@ -20,6 +20,10 @@ final class BerlinClock {
         return singleMinuteRow(for: date).map { $0 ? "Y" : "0" }.joined()
     }
 
+    func fiveMinuteRow(for date: Date) -> String {
+        return "OOOOOOOOOOO"
+    }
+
     private func singleMinuteRow(for date: Date) -> [Bool] {
         let minute = calendar.component(.minute, from: date)
         return singleMinuteRow(for: minute)
@@ -39,6 +43,8 @@ final class BerlinClock {
 
 final class BerlinClockTests: XCTestCase {
     typealias FunctionRow = (Date) -> String
+
+    // MARK: - Single Minutes Row
 
     func test_singleMinuteRow_returnsOOOOEveryFiveMinutesAfter1() {
         assertSingleMinuteRow(every5MinutesAfter: 0, returns: "0000")
@@ -60,6 +66,12 @@ final class BerlinClockTests: XCTestCase {
         assertSingleMinuteRow(every5MinutesAfter: 4, returns: "YYYY")
     }
 
+    // MARK: - Five Minutes Row
+
+    func test_fiveMinuteRow_returnsOOOOOOOOOOOAtMinute0() {
+        assertFiveMinuteRow(at: 0, returns: "OOOOOOOOOOO")
+    }
+
     //MARK - Helpers
 
     func createSut(minute: Int, originalDate: Date = Date()) -> (BerlinClock, Date) {
@@ -69,6 +81,21 @@ final class BerlinClockTests: XCTestCase {
         let time = calendar.date(bySettingHour: 0, minute: minute, second: 0, of: originalDate)!
 
         return (sut, time)
+    }
+
+    func assertFiveMinuteRow(at minute: Int, returns: String, file: StaticString = #file, line: UInt = #line) {
+        let (sut, time) = createSut(minute: minute)
+        let minute: TimeInterval = 60
+
+        let uniqueResults = evaluateFunctionRow(
+            function: sut.fiveMinuteRow,
+            after: time,
+            every: minute,
+            repetitions: 4
+        )
+
+        XCTAssertEqual(uniqueResults.count, 1, file: file, line: line)
+        XCTAssertEqual(uniqueResults.first!, returns, file: file, line: line)
     }
 
     func assertSingleMinuteRow(every5MinutesAfter minute: Int, returns: String, file: StaticString = #file, line: UInt = #line) {
