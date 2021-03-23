de1f7bcba446cf6ef4a6c5591a9949c80d3b71fb
2021-DEV2-012 <80991609+2021-DEV2-012@users.noreply.github.com>
2021-03-24 00:22:46 +0100

    Make side frameworks static as to attempt to improve loading time.

--- BerlinClock/BerlinClockMainUITests/BerlinClockMainUITests.swift
+++ BerlinClock/BerlinClockMainUITests/BerlinClockMainUITests.swift
@@ -59,6 +59,15 @@ class BerlinClockMainUITests: XCTestCase {
         }
     }
 
+    func testLaunchPerformance() throws {
+        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
+            // This measures how long it takes to launch your application.
+            measure(metrics: [XCTApplicationLaunchMetric()]) {
+                XCUIApplication().launch()
+            }
+        }
+    }
+
     // MARK: - Helpers
 
     private func checkVisualExists(identifier: String, on app: XCUIApplication, file: StaticString = #file, line: UInt = #line) {
