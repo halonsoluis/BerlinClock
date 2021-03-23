fd49d6b572085598168d3c45b683904a76d80e2f
2021-DEV2-012 <80991609+2021-DEV2-012@users.noreply.github.com>
2021-03-23 02:33:29 +0100

    Move the CLockViewController into production

---
+++ BerlinClock/BerlinClockUIKit/BerlinClockInteractor.swift
@@ -0,0 +1,13 @@
+//
+//  BerlinClockInteractor.swift
+//  BerlinClockUIKit
+//
+//  Created by Hugo Alonso on 23/03/2021.
+//
+
+import Foundation
+
+protocol BerlinClockInteractor {
+    func start()
+    func stop()
+}
--- BerlinClock/BerlinClockUIKitTests/BerlinClockViewControllerTests.swift
+++ BerlinClock/BerlinClockUIKitTests/BerlinClockViewControllerTests.swift
@@ -7,52 +7,7 @@
 
 import XCTest
 import UIKit
-import BerlinClock
-
-protocol ClockPresenter {
-    func setLampsColor(colors: [UIColor])
-}
-
-final class BerlinClockViewController: UIViewController {
-    private var interactor: BerlinClockInteractor?
-    var lamps: [UIView]?
-
-    convenience init(interactor: BerlinClockInteractor) {
-        self.init()
-        self.interactor = interactor
-    }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        lamps = Array(repeating: UIView(), count: 24)
-    }
-
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-
-        interactor?.start()
-    }
-
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-
-        interactor?.stop()
-    }
-}
-
-extension BerlinClockViewController: ClockPresenter {
-    func setLampsColor(colors: [UIColor]) {
-
-        guard let lamps = lamps else {
-            return
-        }
-
-        zip(colors, lamps).forEach { (color, lamp) in
-            lamp.backgroundColor = color
-        }
-    }
-}
+@testable import BerlinClockUIKit
 
 final class BerlinClockViewControllerTests: XCTestCase {
 
