b2c8121519ad6b0a7b47efeb6ddbf1e7690cd5ed
2021-DEV2-012 <80991609+2021-DEV2-012@users.noreply.github.com>
2021-03-23 04:43:11 +0100

    Setup the MainComposer for seeing everything working in collaboration

--- BerlinClock/BerlinClockMain/MainComposer.swift
+++ BerlinClock/BerlinClockMain/MainComposer.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+import BerlinClock
+import BerlinClockUIKit
+import UIKit
+
+final class MainComposer {
+    private let window: UIWindow
+    
+    init(window: UIWindow) {
+        self.window = window
+    }
+
+    func start() {
+        let berlinClock = BerlinClock.create()
+        let interactor = BerlinClockViewModel(clock: berlinClock)
+
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        guard let clock = storyboard.instantiateViewController(withIdentifier: "Clock") as? BerlinClockViewController else {
+            fatalError("ViewController is not the expected")
+        }
+
+        clock.connect(interactor: interactor)
+        interactor.presenter = clock
+
+        window.rootViewController = clock
+    }
+}
