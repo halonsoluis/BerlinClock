cbd328e3820cfbe12d029cbf7609674fa76fad10
2021-DEV2-012 <80991609+2021-DEV2-012@users.noreply.github.com>
2021-03-23 02:49:23 +0100

    Move ClockViewModel into production for improving reliability of the autocompletion

---
+++ BerlinClock/BerlinClockUIKit/BerlinClockViewModel.swift
@@ -0,0 +1,32 @@
+//
+//  BerlinClockViewModel.swift
+//  BerlinClockUIKit
+//
+//  Created by Hugo Alonso on 23/03/2021.
+//
+
+import Foundation
+import BerlinClock
+
+final class BerlinClockViewModel {
+    private let clock: BerlinClockTimeProvider
+    var ticker: Timer?
+
+    init(clock: BerlinClockTimeProvider) {
+        self.clock = clock
+    }
+
+    @objc func updateTime(timer: Timer) {
+        _ = clock.time(for: Date())
+    }
+}
+
+extension BerlinClockViewModel: BerlinClockInteractor {
+    func start() {
+        ticker = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: updateTime)
+    }
+
+    func stop() {
+        ticker?.invalidate()
+    }
+}
--- BerlinClock/BerlinClockUIKitTests/BerlinClockViewModelTests.swift
+++ BerlinClock/BerlinClockUIKitTests/BerlinClockViewModelTests.swift
@@ -4,33 +4,9 @@
 //
 //  Created by Hugo Alonso on 23/03/2021.
 //
-
 import XCTest
 import BerlinClock
 @testable import BerlinClockUIKit
-
-final class BerlinClockViewModel {
-    private let clock: BerlinClockTimeProvider
-    var ticker: Timer?
-
-    init(clock: BerlinClockTimeProvider) {
-        self.clock = clock
-    }
-
-    @objc func updateTime(timer: Timer) {
-        _ = clock.time(for: Date())
-    }
-}
-
-extension BerlinClockViewModel: BerlinClockInteractor {
-    func start() {
-        ticker = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: updateTime)
-    }
-
-    func stop() {
-        ticker?.invalidate()
-    }
-}
 
 final class BerlinClockViewModelTests: XCTestCase {
    
