1abcfe506c3fedbffdb752e0040d75e89d39dac2
2021-DEV2-012 <80991609+2021-DEV2-012@users.noreply.github.com>
2021-03-23 23:48:24 +0100

    Add accessibility identifier to all the views that are relevant to the clock

--- BerlinClock/BerlinClockMainUITests/BerlinClockMainUITests.swift
+++ BerlinClock/BerlinClockMainUITests/BerlinClockMainUITests.swift
@@ -38,6 +38,7 @@ class BerlinClockMainUITests: XCTestCase {
     func test_visualForHour5_isPresent() throws {
         // UI tests must launch the application that they test.
         let app = XCUIApplication()
+
         app.launch()
         
         // Use recording to get started writing UI tests.
