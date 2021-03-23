82096213d7baff482075f698a427019940e10a30
2021-DEV2-012 <80991609+2021-DEV2-012@users.noreply.github.com>
2021-03-23 20:40:43 +0100

    Extract configuration of the color handling and mapping

--- BerlinClock/BerlinClockMain/MainComposer.swift
+++ BerlinClock/BerlinClockMain/MainComposer.swift
@@ -12,27 +12,37 @@ import UIKit
 
 final class MainComposer {
 
-    func start(using window: UIWindow) {
+    func configureColorHandling() -> (ColorSchema, mapper: (String) -> RGBA) {
         let colors = (yellow: "Y", red: "R", off: "O")
+
         let colorMap: [String: RGBA] = [
             colors.yellow: RGBA(red: 245/255, green: 229/255, blue: 27/255),
             colors.red: RGBA(red: 1, green: 0, blue: 0),
             colors.off: RGBA(red: 0, green: 0, blue: 0, alpha: 0.65)
         ]
-        let berlinClock = BerlinClock.create(
-            colorSchema: ColorSchema(
-                off: colors.off,
-                seconds: colors.yellow,
-                minutes: colors.yellow,
-                minutesVisualAid: colors.red,
-                hours: colors.red
-            )
+
+        let colorSchema = ColorSchema(
+            off: colors.off,
+            seconds: colors.yellow,
+            minutes: colors.yellow,
+            minutesVisualAid: colors.red,
+            hours: colors.red
         )
 
-        let interactor = BerlinClockViewModel(clock: berlinClock) { colorRepresentation in
+        let mapper: (String) -> RGBA = { colorRepresentation in
             let unexpectedStringColor = RGBA(red: 0, green: 0, blue: 0)
             return (colorMap[colorRepresentation] ?? unexpectedStringColor)
         }
+
+        return (colorSchema, mapper)
+    }
+
+    func start(using window: UIWindow) {
+
+        let (schema, mapper) = configureColorHandling()
+
+        let berlinClock = BerlinClock.create(colorSchema: schema)
+        let interactor = BerlinClockViewModel(clock: berlinClock, colorMapper: mapper)
 
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         guard let clockView = storyboard.instantiateViewController(withIdentifier: "Clock") as? BerlinClockViewController else {
