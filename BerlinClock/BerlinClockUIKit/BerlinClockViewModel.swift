7bc3a3eb6f4fbc0033eebc1fd4853e8add72baf3
2021-DEV2-012 <80991609+2021-DEV2-012@users.noreply.github.com>
2021-03-23 03:13:36 +0100

    Use a Date provider via injection to be able to test Date related functionalities

--- BerlinClock/BerlinClockUIKit/BerlinClockViewModel.swift
+++ BerlinClock/BerlinClockUIKit/BerlinClockViewModel.swift
@@ -7,18 +7,33 @@
 
 import Foundation
 import BerlinClock
+import UIKit
 
 final class BerlinClockViewModel {
     private let clock: BerlinClockTimeProvider
+    private let dateProvider: () -> Date
+
+    weak var presenter: ClockPresenter?
     var ticker: Timer?
 
-    init(clock: BerlinClockTimeProvider) {
+    init(clock: BerlinClockTimeProvider, dateProvider: @escaping () -> Date = { Date() }) {
         self.clock = clock
+        self.dateProvider = dateProvider
     }
 
     @objc func updateTime(timer: Timer) {
-        _ = clock.time(for: Date())
-        presenter?.setLampsColor(colors: [])
+        let formattedTime = clock.time(for: dateProvider())
+
+        let colors: [UIColor] = formattedTime.compactMap {
+            switch $0 {
+            case "Y": return UIColor.yellow
+            case "R": return UIColor.red
+            case "0": return UIColor.darkGray
+            default: return nil
+            }
+        }
+
+        presenter?.setLampsColor(colors: colors)
     }
 }
 
