d9023952e7d1a5c481e2990c07175c9037ce82fa
2021-DEV2-012 <80991609+2021-DEV2-012@users.noreply.github.com>
2021-03-23 03:48:21 +0100

    Make the viewmodel agnostic of the technology used on the UI by not using UIKit types

--- BerlinClock/BerlinClockUIKit/BerlinClockViewModel.swift
+++ BerlinClock/BerlinClockUIKit/BerlinClockViewModel.swift
@@ -7,7 +7,13 @@
 
 import Foundation
 import BerlinClock
-import UIKit
+
+struct RGBA: Equatable {
+    let red: Float
+    let green: Float
+    let blue: Float
+    let alpha: Float
+}
 
 final class BerlinClockViewModel {
     private let clock: BerlinClockTimeProvider
@@ -24,15 +30,14 @@ final class BerlinClockViewModel {
     @objc func updateTime(timer: Timer) {
         let formattedTime = clock.time(for: dateProvider())
 
-        let colors: [UIColor] = formattedTime.compactMap {
+        let colors: [RGBA] = formattedTime.compactMap {
             switch $0 {
-            case "Y": return UIColor.yellow
-            case "R": return UIColor.red
-            case "0": return UIColor.darkGray
+            case "Y": return RGBA(red: 0, green: 1, blue: 1, alpha: 1)
+            case "R": return RGBA(red: 1, green: 0, blue: 0, alpha: 1)
+            case "0": return RGBA(red: 0, green: 0, blue: 0, alpha: 0.65)
             default: return nil
             }
         }
-
         presenter?.setLampsColor(colors: colors)
     }
 }
