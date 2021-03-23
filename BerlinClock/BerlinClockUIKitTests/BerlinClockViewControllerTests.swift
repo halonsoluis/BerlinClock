8d017e0774b63eb66e9a6b848060692b49dc54c6
2021-DEV2-012 <80991609+2021-DEV2-012@users.noreply.github.com>
2021-03-23 01:12:35 +0100

    Guarantee that the clock does not start on creation of the ClockViewController class

---
+++ BerlinClock/BerlinClockUIKitTests/BerlinClockViewControllerTests.swift
@@ -0,0 +1,32 @@
+//
+//  BerlinClockViewControllerTests.swift
+//  BerlinClockUIKitTests
+//
+//  Created by ***REMOVED*** on 23/03/2021.
+//
+
+import XCTest
+
+final class BerlinClockViewController {
+    let clock: BerlinClockViewControllerTests.ClockSpy
+
+    init(clock: BerlinClockViewControllerTests.ClockSpy) {
+        self.clock = clock
+    }
+}
+
+final class BerlinClockViewControllerTests: XCTestCase {
+
+    func test_init_doesNotStartTheClock() {
+        let clock = ClockSpy()
+        _ = BerlinClockViewController(clock: clock)
+
+        XCTAssertEqual(clock.formatCallCount, 0)
+    }
+
+    // MARK: - Helpers
+
+    class ClockSpy {
+        private (set) var formatCallCount: Int = 0
+    }
+}
