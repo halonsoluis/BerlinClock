efd5fa0f6e351ba66bf067b9c39bcec308231ced
2021-DEV2-012 <80991609+2021-DEV2-012@users.noreply.github.com>
2021-03-23 04:43:11 +0100

    Setup the MainComposer for seeing everything working in collaboration

--- BerlinClock/BerlinClockMain/SceneDelegate.swift
+++ BerlinClock/BerlinClockMain/SceneDelegate.swift
@@ -16,7 +16,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+
+        // 2. Create a new UIWindow using the windowScene constructor which takes in a window scene.
+        let window = UIWindow(windowScene: windowScene)
+
+        /// 5. Set the window and call makeKeyAndVisible()
+        self.window = window
+        window.makeKeyAndVisible()
+
+        MainComposer(window: window).start()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
