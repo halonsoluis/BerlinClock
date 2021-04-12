45c3571e15507e66a9475efa8e84cbb8f2dd4f67
Hugo Alonso Luis <hugoalonsoluis@gmail.com>
2021-04-12 18:36:58 +0200

    Assert that TimerFactoryImplementation handles the timer as expected, invalidating it on stop and creating it on execute with the provided interval

---
+++ BerlinClock/BerlinClockUIKitTests/TickerFactoryImplementationTests.swift
@@ -0,0 +1,35 @@
+//
+//  TickerFactoryImplementationTests.swift
+//  BerlinClockUIKitTests
+//
+//  Created by Hugo Alonso on 12/04/2021.
+//
+
+import XCTest
+@testable import BerlinClockUIKit
+
+final class TickerFactoryImplementationTests: XCTestCase {
+
+    func test_execute_createsValidTimer() {
+        let tickerFactory = createSUT()
+
+        let timer = tickerFactory.execute(block: {}, every: 10)
+
+        XCTAssertTrue(timer.isValid)
+        XCTAssertEqual(timer.timeInterval, 10)
+    }
+
+    func test_stop_invalidatesTheTimer() {
+        let tickerFactory = createSUT()
+
+        let timer = tickerFactory.execute(block: {}, every: 10)
+
+        tickerFactory.stop(ticker: timer)
+        XCTAssertFalse(timer.isValid)
+    }
+
+    func createSUT() -> TickerFactory {
+        return TickerFactoryImplementation()
+    }
+}
+
